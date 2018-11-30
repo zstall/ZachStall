db63243e09a72de68a37bb9215ac03da42601e13
zstall <zstall4@gmail.com>
2018-11-30 11:44:37 -0700

    Last update before taking on blog

--- ZachStallPortfolio/ZachStallPortfolio/views.py
+++ ZachStallPortfolio/ZachStallPortfolio/views.py
@@ -1,13 +1,13 @@
 from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
-    template_name = 'index.html'
+    template_name = 'ZSP/index.html'
 
 class AboutMePage(TemplateView):
-    template_name = 'about_me.html'
+    template_name = 'ZSP/about_me.html'
 
 class PcBuildsPage(TemplateView):
-    template_name = 'pc_builds.html'
+    template_name = 'ZSP/pc_builds.html'
 
 class LeelaPage(TemplateView):
-    template_name = 'leela.html'
+    template_name = 'ZSP/leela.html'
