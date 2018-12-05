bcd12a9464591b969e91f08c2251f125dfb7ae69
zach_mac <zstall4@gmail.com>
2018-12-04 18:41:28 -0700

    trying aws s3 storage

--- ZachStallPortfolio/ZachStallPortfolio/settings.py
+++ ZachStallPortfolio/ZachStallPortfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'embed_video',
     'bootstrap3',
     'zBLOG',
--- ZachStallPortfolio/zBLOG/models.py
+++ ZachStallPortfolio/zBLOG/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length = 200)
     text = models.TextField()
-    blog_image = models.ImageField( upload_to="media/zBLOG/upload_images",
+    blog_image = models.ImageField( upload_to="upload_images",
                                     default = "/zBLOG/upload_images/zBlog_index_image.jpg")
     post_video = EmbedVideoField(null=True, blank = True)
     create_date = models.DateTimeField(default = timezone.now)
