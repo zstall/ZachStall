a99a928002cb44abd65d06a9d350e38a0fef0e34
zstall <zstall4@gmail.com>
2018-12-06 15:23:29 -0700

    lightbox updates

--- ZachStallPortfolio/static/ZachStallPortfolio/js/master.js
+++ ZachStallPortfolio/static/ZachStallPortfolio/js/master.js
@@ -47,41 +47,37 @@ $(window).scroll(function() {
   // Modal Js
   // ****************************************************************************
   // Open the Modal
-  function openModal() {
-    document.getElementById('myModal').style.display = "block";
-  }
-
-  // Close the Modal
-  function openModal() {
-    document.getElementById('myModal').style.display = "block";
+  function openModal(id) {
+    document.getElementById(id).style.display = "block";
   }
 
   function closeModal() {
     document.getElementById('myModal').style.display = "none";
+    document.getElementById('myModal1').style.display = "none";
   }
 
   var slideIndex = 1;
   showSlides(slideIndex);
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+  function plusSlides(n,name,thumb) {
+    showSlides(slideIndex += n,cls=name,thm=thumb);
   }
 
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  function currentSlide(n,name,thumb) {
+    showSlides(slideIndex = n,cls=name,thm=thumb);
   }
 
-  function showSlides(n) {
+  function showSlides(n,cls,thm) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var slides = document.getElementsByClassName(cls);
+    var dots = document.getElementsByClassName(thm);
     var captionText = document.getElementById("caption");
-    if (n > slides.length-24) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length-24}
-    for (i = 0; i < slides.length-24; i++) {
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length-24; i++) {
+    for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";
